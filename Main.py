48c0850b9f79d9d27b6d1ab25dac6fb2d0a415a2
Leon-Parepko <l.parepko@innopolis.university>
2021-12-21 18:59:57 +0600

    added change prefix command, change superuser(NOT WORK, COMENTED)

--- Main.py
+++ Main.py
@@ -1,31 +1,61 @@
+from discord.ext.commands import has_permissions
+
 from discord import *
+from discord.ext import commands
+import discord.errors
 import discord
+# import asyncio
 import sys
 import logging
+# import functional
 
 
 
+#--------------------------VARIABLES & CLASSES INIT----------------------------------
+file_path = "C:\\Users\\Leon\\Desktop\\Discord-Bot\\config.txt"
+prefixes_available = "!@#$%^&*+_-~`=:;?/.<>,|"
+logging.basicConfig(filename='logging.log', level=logging.INFO, filemode='w', format='%(levelname)s   -   %(asctime)s   -   %(message)s')
 
-
-#-----------------VARIABLES & CLASSES INIT------------------
-logging.basicConfig(filename='logging.log', filemode='w', format='%(levelname)s   -   %(asctime)s   -   %(message)s')
-
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    logging.error("Logged on as {0}".format(client.user)) # Some problems with this (it is not errors!)
-
-
+command_index = ""
 
 class Config:
-    def __init__(self, token, prefix, test_1):
+    def __init__(self, token, prefix, available_channels, super_user):
         self.token = token
         self.prefix = prefix
-        self.test_1 = test_1
+        self.available_channels = available_channels
+        self.super_user = super_user
 
 
 
+
+
+
+#--------------------------SUBSIDIARY_FUNCTIONS----------------------------------
+def parse_file(file_path):
+    content = []
+    f = open(file_path, "r")
+    content_raw = f.read().split("\n")
+    f.close()
+    for i in content_raw:
+        content.append(i.split(" = ")[1])
+    return content
+
+def reconfig(file_content):
+    config.token = file_content[0]
+    config.prefix = file_content[1]
+    config.available_channels = str(file_content[2]).split(" ")
+    config.super_user = str(file_content[3]).split(" ")
+
+def prefix_check(prefix):
+    if prefix in prefixes_available:
+        return True
+    else:
+        return False
+
+# def bot_upd_config():
+
+def valid_user_check(user):
+    pass
 
 
 
@@ -39,39 +69,102 @@ class CustomError(Error):
 
 
 
-#-----------------MAIN------------------
+#------------------------------MAIN-----------------------------
+
+
+
+
+
 try:
-
-    f = open("C:\\Users\\Leon\\Desktop\\Discord-Bot\\config.txt", "r")
-    content = f.read().split("\n")
-
-    config = Config(content[0], content[1], content[2])
-
-
-
-    @client.event
-    async def on_message(message):
-        if message.content.startswith("#Test"):
-            await message.channel.send(message.content.split(" ")[1])
+#Config initialization
+    content = parse_file(file_path)
+    config = Config(content[0],
+                    content[1],
+                    str(content[2]).split(" "),
+                    str(content[3]).split(" "))
 
 
 
+    bot = commands.Bot(command_prefix=config.prefix)
+
+
+
+    @bot.event
+    async def on_ready():
+        logging.info("Logged on as {0}".format(bot.user))
 
 
 
 
-    client.run(config.token)
+    @bot.command()
+    @has_permissions(administrator=True)
+    async def prefix_config(ctx, arg):
+        if prefix_check(str(arg)):
+            file_reader = open(file_path, "r")
+            lines_list = file_reader.readlines()
+            file_reader.close()
+            lines_list[1] = "PREFIX = " + str(arg) + "\n"
+            file_writer = open(file_path, "w")
+            file_writer.writelines(lines_list)
+            file_writer.close()
+            reconfig(parse_file(file_path))
+            bot.command_prefix = str(arg)
+            await ctx.message.channel.send("Prefix has been changed successfully.")
+        else:
+            await ctx.message.channel.send("This prefix is invalid! Please, try one of this - ```" + prefixes_available + "```\nExample: " + config.prefix + "prefix_config PREFIX")
 
 
+    # @bot.command()
+    # @has_permissions(administrator=True)
+    # async def superuser_config(ctx, operation, arg):
+    #
+    #
+    #     if valid_user_check(ctx.user) == True:
+    #         if operation == "add":
+    #             pass
+    #         elif operation == "set":
+    #             pass
+    #         else:
+    #             await ctx.message.channel.send("This operation is invalid! Please, try one of this - set, add \nExample: " + config.prefix + "superuser_config set USER")
+    #
+    #         if prefix_check(str(arg)):
+    #             file_reader = open(file_path, "r")
+    #             lines_list = file_reader.readlines()
+    #             file_reader.close()
+    #             lines_list[1] = "PREFIX = " + str(arg) + "\n"
+    #             file_writer = open(file_path, "w")
+    #             file_writer.writelines(lines_list)
+    #             file_writer.close()
+    #             reconfig(parse_file(file_path))
+    #             await ctx.message.channel.send("Prefix changed successfully.")
+    #         else:
+    #             await ctx.message.channel.send("This prefix is invalid! Please, try one of this - " + prefixes_available + "\nExample: " + config.prefix + "prefix_config PREFIX")
+    #     else:
+    #         await ctx.message.channel.send("This user is invalid! Please, try other one\nExample: " + config.prefix + "superuser_config set USER")
+    #
+    #
+
+
+
+
+    bot.run(config.token)
 
 #-----------------ERROR CATCHER------------------
 except FileNotFoundError as e:
     logging.error(e)
+    client.close()
+    sys.exit()
+
+except DiscordException as e:
+    logging.error(e)
+    client.close()
     sys.exit()
 
 except IndexError as e:
     logging.error(e)
+    client.close()
     sys.exit()
 
 else:
+    client.close()
     sys.exit()
---
+++ functional.py
@@ -0,0 +1,16 @@
+import numpy
+
+def channels_config():
+    pass
+
+def prefix_config():
+    pass
+
+def roulette():
+    pass
+    #
+    # items = {"double_coins":0.2, "hardmute_1":0.001,}
+    #
+    # test1 = "aaaa", "bbbb", "cccc"
+    # test2 = [0.5, 0.25, 0.1]
+    # print (numpy.random.choice(test1, p=test2))
